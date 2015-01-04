3317cb58e82e7eeb9169c1bb04c9ba93cbb14ff3
Stephen <stephencwelch@gmail.com>
2015-01-03 19:02:24 -0500

    Added import of numpy

--- partOne.py
+++ partOne.py
@@ -6,6 +6,8 @@
 # Stephen Welch
 # @stephencwelch
 
+import numpy as np
+
 # X = (hours sleeping, hours studying), y = Score on test
 X = np.array(([3,5], [5,1], [10,2]), dtype=float)
 y = np.array(([75], [82], [93]), dtype=float)
