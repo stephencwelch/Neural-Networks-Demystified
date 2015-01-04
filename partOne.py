1be0a5a1a457e69fb8334224358956b810917f2b
Stephen <stephencwelch@gmail.com>
2015-01-03 19:00:15 -0500

    Added partOne.py

---
+++ partOne.py
@@ -0,0 +1,15 @@
+# Neural Networks Demystified
+# Part 1: Data + Architecture
+#
+# Supporting code for short YouTube series on artificial neural networks.
+#
+# Stephen Welch
+# @stephencwelch
+
+# X = (hours sleeping, hours studying), y = Score on test
+X = np.array(([3,5], [5,1], [10,2]), dtype=float)
+y = np.array(([75], [82], [93]), dtype=float)
+
+# Normalize
+X = X/np.amax(X, axis=0)
+y = y/100 #Max test score is 100
