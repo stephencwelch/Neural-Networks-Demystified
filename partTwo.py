5c13b73afc4b9a5cd174e0503dc720f9db8e9e1c
Stephen <stephencwelch@gmail.com>
2015-01-03 19:11:37 -0500

    Finished Part 2

---
+++ partTwo.py
@@ -0,0 +1,44 @@
+# Neural Networks Demystified
+# Part 2: Forward Propagation
+#
+# Supporting code for short YouTube series on artificial neural networks.
+#
+# Stephen Welch
+# @stephencwelch
+
+
+## ----------------------- Part 1 ---------------------------- ##
+import numpy as np
+
+# X = (hours sleeping, hours studying), y = Score on test
+X = np.array(([3,5], [5,1], [10,2]), dtype=float)
+y = np.array(([75], [82], [93]), dtype=float)
+
+# Normalize
+X = X/np.amax(X, axis=0)
+y = y/100 #Max test score is 100
+
+## ----------------------- Part 2 ---------------------------- ##
+
+class Neural_Network(object):
+    def __init__(self):        
+        #Define Hyperparameters
+        self.inputLayerSize = 2
+        self.outputLayerSize = 1
+        self.hiddenLayerSize = 3
+        
+        #Weights (parameters)
+        self.W1 = np.random.randn(self.inputLayerSize, self.hiddenLayerSize)
+        self.W2 = np.random.randn(self.hiddenLayerSize, self.outputLayerSize)
+        
+    def forward(self, X):
+        #Propagate inputs though network
+        self.z2 = np.dot(X, self.W1)
+        self.a2 = self.sigmoid(self.z2)
+        self.z3 = np.dot(self.a2, self.W2)
+        yHat = self.sigmoid(self.z3) 
+        return yHat
+        
+    def sigmoid(self, z):
+        #Apply sigmoid activation function to scalar, vector, or matrix
+        return 1/(1+np.exp(-z))
