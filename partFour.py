e47a4241cd1cfce96dc62e3ac3b4b9206f717293
stephencwelch <stephencwelch@gmail.com>
2015-01-04 20:25:30 -0500

    Finished Part 4

---
+++ partFour.py
@@ -0,0 +1,67 @@
+# Neural Networks Demystified
+# Part 2: Forward Propagation
+#
+# Supporting code for short YouTube series on artificial neural networks.
+#
+# Stephen Welch
+# @stephencwelch
+
+
+## ----------------------- Part 1 ---------------------------- ##
+import numpy as np
+
+# X = (hours sleeping, hours studying), y = Score on test
+X = np.array(([3,5], [5,1], [10,2]), dtype=float)
+y = np.array(([75], [82], [93]), dtype=float)
+
+# Normalize
+X = X/np.amax(X, axis=0)
+y = y/100 #Max test score is 100
+
+## ----------------------- Part 4 ---------------------------- ##
+
+# Whole Class with additions:
+class Neural_Network(object):
+    def __init__(self):        
+        #Define Hyperparameters
+        self.inputLayerSize = 2
+        self.outputLayerSize = 1
+        self.hiddenLayerSize = 3
+        
+        #Weights (parameters)
+        self.W1 = np.random.randn(self.inputLayerSize,self.hiddenLayerSize)
+        self.W2 = np.random.randn(self.hiddenLayerSize,self.outputLayerSize)
+        
+    def forward(self, X):
+        #Propogate inputs though network
+        self.z2 = np.dot(X, self.W1)
+        self.a2 = self.sigmoid(self.z2)
+        self.z3 = np.dot(self.a2, self.W2)
+        yHat = self.sigmoid(self.z3) 
+        return yHat
+        
+    def sigmoid(self, z):
+        #Apply sigmoid activation function to scalar, vector, or matrix
+        return 1/(1+np.exp(-z))
+    
+    def sigmoidPrime(self,z):
+        #Gradient of sigmoid
+        return np.exp(-z)/((1+np.exp(-z))**2)
+    
+    def costFunction(self, X, y):
+        #Compute cost for given X,y, use weights already stored in class.
+        self.yHat = self.forward(X)
+        J = 0.5*sum((y-self.yHat)**2)
+        return J
+        
+    def costFunctionPrime(self, X, y):
+        #Compute derivative with respect to W and W2 for a given X and y:
+        self.yHat = self.forward(X)
+        
+        delta3 = np.multiply(-(y-self.yHat), self.sigmoidPrime(self.z3))
+        dJdW2 = np.dot(self.a2.T, delta3)
+        
+        delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrime(self.z2)
+        dJdW1 = np.dot(X.T, delta2)  
+        
+        return dJdW1, dJdW2
